4f4aae523294358442fca1bc41020ba2550d7b26
Jhe0031 <jackie.he8@gmail.com>
2019-03-12 16:00:51 -0400

    Added check for lower-case letters

--- transcriptase.cpp
+++ transcriptase.cpp
@@ -13,13 +13,13 @@ int main() {
 	string strand; //Creates integer to input string into
 	while(getline(fin, strand)) { //Reads file and inputs into string strand
 	    for (int i = 0; i < strand.length(); i++) { //Sets for loop
-	    	if (strand[i] == 'A') { //Checks if the letter is A
+	    	if (strand[i] == 'A' || strand[i] == 'a') { //Checks if the letter is A
 	    		cout << 'U'; //Changes letter to U
-	    	} else if (strand[i] == 'T') { //Checks if the letter is T
+	    	} else if (strand[i] == 'T' || strand[i] == 't') { //Checks if the letter is T
 	    		cout << 'A'; //Changes letter to A
-	    	} else if (strand[i] == 'C') { //Checks if the letter is C
+	    	} else if (strand[i] == 'C' || strand[i] == 'c') { //Checks if the letter is C
 	    		cout << 'G'; //Changes letter to G
-	    	} else if (strand[i] == 'G') { //Checks if the letter is G
+	    	} else if (strand[i] == 'G' || strand[i] == 'g') { //Checks if the letter is G
 	    		cout << 'C'; //Changes leter to C
 	    	} else { //Otherwise
 	    		cout << "\n"; //Prints a new line

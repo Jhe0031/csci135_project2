e3dca4315ff6759e01218339d449d5f49dc67419
Jhe0031 <jackie.he8@gmail.com>
2019-03-12 15:59:06 -0400

    Added commentary

--- transcriptase.cpp
+++ transcriptase.cpp
@@ -1,33 +1,32 @@
-#include <iostream>
-#include <fstream>
+#include <iostream> 
+#include <fstream> 
 #include <cstdlib>
 
-using namespace std;
+using namespace std; 
 
 int main() {
-	ifstream fin("dna.txt");
-	if (fin.fail()) {
-	    cerr << "File cannot be read, opened, or does not exist.\n";
-	    exit(1);
+	ifstream fin("dna.txt"); //Opens file
+	if (fin.fail()) { //Sets fail code
+	    cerr << "File cannot be read, opened, or does not exist.\n"; //Prints fail message
+	    exit(1); //Exits fail code
 	}
-	string strand;
-	while(getline(fin, strand)) {
-	    // cout << strand << endl;
-	    for (int i = 0; i < strand.length(); i++) {
-	    	if (strand[i] == 'A') {
-	    		cout << 'U';
-	    	} else if (strand[i] == 'T') {
-	    		cout << 'A';
-	    	} else if (strand[i] == 'C') {
-	    		cout << 'G';
-	    	} else if (strand[i] == 'G') {
-	    		cout << 'C';
-	    	} else {
-	    		cout << "\n";
+	string strand; //Creates integer to input string into
+	while(getline(fin, strand)) { //Reads file and inputs into string strand
+	    for (int i = 0; i < strand.length(); i++) { //Sets for loop
+	    	if (strand[i] == 'A') { //Checks if the letter is A
+	    		cout << 'U'; //Changes letter to U
+	    	} else if (strand[i] == 'T') { //Checks if the letter is T
+	    		cout << 'A'; //Changes letter to A
+	    	} else if (strand[i] == 'C') { //Checks if the letter is C
+	    		cout << 'G'; //Changes letter to G
+	    	} else if (strand[i] == 'G') { //Checks if the letter is G
+	    		cout << 'C'; //Changes leter to C
+	    	} else { //Otherwise
+	    		cout << "\n"; //Prints a new line
 	    	}
 	    }
-	    cout << "\n";
+	    cout << "\n"; //Prints a new line when string reaches its end
 	}
-	fin.close();
+	fin.close(); //Closes file
 	return 0;
 }

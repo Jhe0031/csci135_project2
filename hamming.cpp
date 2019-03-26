82ca143235ec9a5a5d7e61cae2e6b64163166fb5
Jhe0031 <jackie.he8@gmail.com>
2019-03-26 16:00:16 -0400

    Added more commentary

--- hamming.cpp
+++ hamming.cpp
@@ -12,20 +12,20 @@ int main() {
 	    cerr << "File cannot be read, opened, or does not exist.\n"; //Prints fail message
 	    exit(1); //Exits fail code
 	}
-	string mut, orig;
-	while (getline (fin, mut), getline (fin, orig)) {
-		int count = 0;
-		for (int i = 0; i < 45; i++) {
-			if (mut[i] != orig[i]) {
-				count += 1;
+	string mut, orig; // Creates string variables to hold the two lines
+	while (getline (fin, mut), getline (fin, orig)) { // Takes in the two lines
+		int count = 0; // Creates a variable to count the amount of changes
+		for (int i = 0; i < 45; i++) { // For loop 
+			if (mut[i] != orig[i]) { 
+				count += 1; // Adds one for every substitution
 			}
 		}
-		if (count > 0) {
-			cout << count << " yes" << endl;
-			count = 0;
-		} else {
-			cout << 0 << " no" << endl;
-			count = 0;
+		if (count > 0) { 
+			cout << count << " yes" << endl; // Ouputs the value of count in addition to whether or not there is a mutation
+			count = 0; // Resets the count value
+		} else { 
+			cout << 0 << " no" << endl; // Ouputs the value of count in addition to whether or not there is a mutation
+			count = 0; // Resets the count value
 		}
 	}
 	return 0;

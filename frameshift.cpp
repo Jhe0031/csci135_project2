a5ce9c3001e7c29a5802c0023121fea50856274a
Jhe0031 <jackie.he8@gmail.com>
2019-04-03 12:33:30 -0400

    Clean up code and retry

--- frameshift.cpp
+++ frameshift.cpp
@@ -30,53 +30,45 @@ string codon(string new_s, ifstream &dict) {
 	//     cerr << "codons.tsv: File cannot be read, opened, or does not exist.\n"; //Prints fail message
 	//     // exit(1); //Exits fail code
 	// }
-	int j = 0;
+	int j;
 	string codon = "";
 	string three = "";
 	for (int i = 0; i < new_s.length(); i++) {
-		if (new_s[i] == char(65)) {
-			if (new_s[i+1] == char(85) && new_s[i+2] == char(71)) {
-				j = i;
-					while (j < new_s.length()) {
-						for (int k = 0; k < 3; k++) {
-							three += new_s[j];
-							j += 1;
-						}
-						if (three == "AUG") {
-							while (j < new_s.length()) {
-								if (three == "UAA" || three == "UGA" || three == "UAG") {
-									codon = codon.substr(0, codon.size()-1);
-									codon += "\n";
-									three = "";
-									break;
-								}
-								string key, value;
-								dict.clear(); // reset error state
-								dict.seekg(0); // return file pointer to the beginning
-								while (dict >> key >> value) {
-									if (key == three) {
-										codon += value;
-										codon += "-";
-									}
-								}
-								three = "";
-								for (int k = 0; k < 3; k++) {
-									three += new_s[j];
-									j += 1; 
-								}
-							}
-						} else {
-							three = "";
-						}
-						if (j >= new_s.length()) {
-							codon = codon.substr(0, codon.size()-1);
-						}
-					}
-					// dict.close();
-					return codon;
-				}
-			} 
+		if (new_s[i] == char(65) && new_s[i+1] == char(85) && new_s[i+2] == char(71)) {
+			j = i;
+			break;
 		}
+	}
+	while (j < new_s.length()) {
+		for (int k = 0; k < 3; k++) {
+			three += new_s[j];
+			j += 1;
+		}
+		if (three == "UAA" || three == "UGA" || three == "UAG") {
+			codon = codon.substr(0, codon.size()-1);
+			// codon += "\n";
+			three = "";
+			break;
+		}
+		string key, value;
+		dict.clear(); // reset error state
+		dict.seekg(0); // return file pointer to the beginning
+		while (dict >> key >> value) {
+			if (key == three) {
+				codon += value;
+				codon += "-";
+			}
+		}
+		three = "";
+		// for (int k = 0; k < 3; k++) {
+		// 	three += new_s[j];
+		// 	j += 1; 
+		// }
+		// if (j >= new_s.length()) {
+		// 	codon = codon.substr(0, codon.size()-1);
+		// }
+	}
+	return codon;
 }
 
 int main() {
